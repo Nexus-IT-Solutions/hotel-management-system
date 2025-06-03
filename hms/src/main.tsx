47a3bda5bb6663948e268dfc7fbd8d1d8b0f6839
Afriyie-Anthony <kobbydalton@icloud.com>
2025-06-03 09:00:05 +0000

    feat: add customer page route to the application

--- hms/src/main.tsx
+++ hms/src/main.tsx
@@ -5,6 +5,7 @@ import MainLayout from './components/layout/MainLayout.tsx'
 import Dashboard from './pages/admin/Dashboard.tsx'
 import NewBooking from './pages/admin/NewBooking.tsx'
 import Bookings from './pages/admin/Bookings.tsx'
+import Customer from './pages/admin/Customer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -13,7 +14,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/new-booking' element={<NewBooking/>}/>
         <Route path='/bookings' element={<Bookings/>}/>
+        <Route path='/customer' element={<Customer/>}/>
       </Route>
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 )

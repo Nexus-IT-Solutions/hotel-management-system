3f6e9f30bdecd7b8dfb6e04718a12ca4465ecc6b
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-06-04 23:37:53 +0000

    refactor: remove static secret variable in JwtHelper and use environment variable directly for JWT encoding and decoding

--- backend/src/helper/JwtHelper.php
+++ backend/src/helper/JwtHelper.php
@@ -7,7 +7,6 @@ use Firebase\JWT\Key;
 
 class JwtHelper
 {
-    private static $secret = $_ENV['JWT_SECRET']; 
 
     public static function generate(array $payload, int $expirySeconds = $_ENV['JWT_EXPIRY']): string
     {
@@ -15,13 +14,13 @@ class JwtHelper
         $payload['iat'] = $issuedAt;
         $payload['exp'] = $issuedAt + $expirySeconds;
 
-        return JWT::encode($payload, self::$secret, $_ENV['JWT_ALGORITHM']);
+        return JWT::encode($payload, $_ENV['JWT_SECRET'], $_ENV['JWT_ALGORITHM']);
     }
 
     public static function validate(string $token): ?array
     {
         try {
-            return (array) JWT::decode($token, new Key(self::$secret, $_ENV['JWT_ALGORITHM']));
+            return (array) JWT::decode($token, new Key($_ENV['JWT_SECRET'], $_ENV['JWT_ALGORITHM']));
         } catch (\Exception $e) {
             return null;
         }

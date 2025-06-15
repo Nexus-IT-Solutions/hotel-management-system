ef39817b85a67b7af66e6d696afdf692c21a4d2b
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-06-15 00:34:21 +0000

    refactor: Consolidate footer into main content area for improved layout

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -24,16 +24,17 @@ const MainLayout = () => {
 
       {/* Main Content Area - Only this will scroll */}
       <main className="flex-1 overflow-y-auto px-5 py-3">
-        <Outlet />
+          <Outlet />
+          <footer className="bg-white text-slate-500 py-1">
+            <div className="text-center text-[10px] flex items-center justify-around">
+            <p>&copy; {new Date().getFullYear()} Hotel Management System</p>
+            <p>Developed by: Nolex Prime <p>Contact Us: 0550807914/0541436414</p></p>
+            </div>
+          </footer>
       </main>
 
       {/* Footer - Will stick to bottom */}
-      <footer className="bg-white text-slate-500 py-1 shrink-0">
-        <div className="text-center text-[13px] flex items-center justify-around">
-        <p>&copy; {new Date().getFullYear()} Hotel Management System</p>
-        <p>Developed by: Nolex Prime <p>Contact Us: 0550807914/0541436414</p></p>
-        </div>
-      </footer>
+      
       </div>
     </div>
   );
